ac837e72ca9908f6c0609e1186a76a33b71e8c10
flori <florian.fourquenay@outlook.com>
2025-05-01 09:41:59 +0200

    compilation correction

--- src/main/java/fr/florianpal/fauction/objects/BarrierMenu.java
+++ src/main/java/fr/florianpal/fauction/objects/BarrierMenu.java
@@ -9,13 +9,13 @@ public class BarrierMenu extends Barrier {
     private final String id;
 
     public BarrierMenu(int index, Material material, String title, List<String> description, String texture, int customModelData, String id) {
-        super(index, material, title, description, texture, customModelData);
+        super(index, material, title, description, null, texture, customModelData);
 
         this.id = id;
     }
 
     public BarrierMenu(int index, Material material, String title, List<String> description, BarrierMenu remplacement, String texture, int customModelData, String id) {
-        super(index, material, title, description, remplacement, texture, customModelData);
+        super(index, material, title, description, null, remplacement, texture, customModelData);
 
         this.id = id;
     }
--- src/main/java/fr/florianpal/fauction/objects/BarrierWithCategory.java
+++ src/main/java/fr/florianpal/fauction/objects/BarrierWithCategory.java
@@ -9,13 +9,13 @@ public class BarrierWithCategory extends Barrier {
     private final Category category;
 
     public BarrierWithCategory(int index, Material material, String title, List<String> description, String texture, int customModelData, Category category) {
-        super(index, material, title, description, texture, customModelData);
+        super(index, material, title, description, null, texture, customModelData);
 
         this.category = category;
     }
 
     public BarrierWithCategory(int index, Material material, String title, List<String> description, BarrierWithCategory remplacement, String texture, int customModelData, Category category) {
-        super(index, material, title, description, remplacement, texture, customModelData);
+        super(index, material, title, description, null, remplacement, texture, customModelData);
 
         this.category = category;
     }
